12c388ce820e15931d2a3c7dfc68dfe56de3746e
jumbolimbo <thoffmann706@gmail.com>
2015-09-25 20:15:42 -0400

    Created a TestExample in the new caftc package.

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/TestExample.java
@@ -0,0 +1,21 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.caftc;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Thomas_Hoffmann on 9/25/2015.
+ */
+public class TestExample extends OpMode{
+    @Override
+    public void init(){
+
+    }
+    @Override
+    public void loop(){
+
+    }
+    @Override
+    public void stop(){
+
+    }
+}
