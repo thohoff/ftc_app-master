a624b51a712c7d9d78be74d1ac37bb84315f35bb
jumbolimbo <thoffmann706@gmail.com>
2015-11-29 19:55:11 -0500

    Branched and added libgdx.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/MCAuto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/MCAuto.java
@@ -1,15 +1,16 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.caftc;
+import com.badlogic.gdx.math.Vector2;
 //#theRealJam
 /**
  * Created by Thomas_Hoffmann on 11/29/2015.
  * This is #theRealJam
  */
 public class MCAuto extends BasicAutonomous{
-  @Override
+ // @Override
   public void init(){
-
+    Vector2 v = new Vector2(0 , 1);
   }
-   @Override
+ //  @Override
     public void loop(){
 
     }
