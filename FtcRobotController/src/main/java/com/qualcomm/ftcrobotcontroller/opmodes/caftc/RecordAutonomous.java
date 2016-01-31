37b305bbea3998020446fe0936e4877e9f6aaa79
jumbolimbo <thoffmann706@gmail.com>
2016-01-31 08:39:54 -0500

    Added the files to start the Recorded Autonomous

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/RecordAutonomous.java
@@ -0,0 +1,24 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.caftc;
+
+/**
+ * Created by Thomas_Hoffmann on 1/31/2016.
+ * This will store the robot's movements, determined by a human controller, in a RecordedMoves object.
+ */
+public class RecordAutonomous extends BasicAutonomous{
+    @Override
+    public void init(){
+        super.init();
+    }
+    @Override
+    public void start() {
+
+    }
+    @Override
+    public void loop() {
+
+    }
+    @Override
+    public void stop(){
+
+    }
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/RecordedMoves.java
@@ -0,0 +1,7 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.caftc;
+
+/**
+ * Created by Thomas_Hoffmann on 1/31/2016.
+ */
+public class RecordedMoves {
+}
