0514d25989c10c68821b3556e71a2294e5f6740a
Ben_Humphries <ben_humphries@caryacademy.org>
2016-02-03 07:12:52 -0500

    testing commit changes

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/RecordAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/RecordAutonomous.java
@@ -1,17 +1,27 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.caftc;
 
+import java.io.File;
+import java.io.OutputStreamWriter;
+import android.content.Context;
+import android.os.Environment;
+
 /**
  * Created by Thomas_Hoffmann on 1/31/2016.
  * This will store the robot's movements, determined by a human controller, in a RecordedMoves object.
  */
 public class RecordAutonomous extends BasicAutonomous{
+
+    public String basePath = "/data/data/caftc/files/";
+    private String path;
+
+    private String filename; //example "autonomous1.txt"
+
     @Override
     public void init(){
         super.init();
     }
     @Override
-    public void start() {
-
+    public void start(){
     }
     @Override
     public void loop() {
@@ -20,5 +30,9 @@ public class RecordAutonomous extends BasicAutonomous{
     @Override
     public void stop(){
 
+    }
+    private void WriteToFile(String filename, String input){
+
+        
     }
 }
