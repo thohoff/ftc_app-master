3173fdbe440836bd743a03bfedf01e2d5a5a7a7e
Ben_Humphries <ben_humphries@caryacademy.org>
2016-02-03 10:16:59 -0500

    Implemented Read and Write methods to files.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/RecordAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/RecordAutonomous.java
@@ -1,18 +1,18 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.caftc;
 
+
 import java.io.File;
-import java.io.OutputStreamWriter;
-import android.content.Context;
-import android.os.Environment;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  * Created by Thomas_Hoffmann on 1/31/2016.
  * This will store the robot's movements, determined by a human controller, in a RecordedMoves object.
  */
 public class RecordAutonomous extends BasicAutonomous{
-
-    public String basePath = "/data/data/caftc/files/";
-    private String path;
+    
 
     private String filename; //example "autonomous1.txt"
 
@@ -21,7 +21,9 @@ public class RecordAutonomous extends BasicAutonomous{
         super.init();
     }
     @Override
+
     public void start(){
+
     }
     @Override
     public void loop() {
@@ -31,8 +33,24 @@ public class RecordAutonomous extends BasicAutonomous{
     public void stop(){
 
     }
-    private void WriteToFile(String filename, String input){
+    private void WriteFile(String filename, String input) throws IOException{
 
-        
+        PrintWriter printWriter = new PrintWriter(filename);
+
+        printWriter.println(input);
+        printWriter.close();
+    }
+    private String ReadFile(String filename) throws IOException{
+
+        String recording = "";
+
+        File file = new File(filename);
+        Scanner reader = new Scanner(file);
+
+        while(reader.hasNext()){
+            recording += reader.nextLine();
+        }
+
+        return recording;
     }
 }
