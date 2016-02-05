1e8e4762c35c546f388e848e3c2f96dc2565110c
Ben_Humphries <ben_humphries@caryacademy.org>
2016-02-05 05:56:01 -0500

    Added a motor class to manage states, tested for a change in states in loop(). There is a comment explaining my plan for the program now as well.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/RecordAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/RecordAutonomous.java
@@ -18,29 +18,12 @@ public class RecordAutonomous extends BasicHardware{
     private String filename; //example "autonomous1.txt"
     private String recording;
 
-    private enum MotorState {
-        FORWARD (0f),
-        BACKWARD (0f),
-        INACTIVE (0f);
 
-        private float time; //Time in milliseconds
+    Motor leftMotor;
+    Motor rightMotor;
 
-        private MotorState(float time){
-            this.time = time;
-        }
-
-        public void AddTime(float time){
-            this.time += time;
-        }
-
-        public float GetTime(){
-            return time;
-        }
-
-    }
-
-    MotorState leftMotor;
-    MotorState rightMotor;
+    float leftMotorTime;  //Time in ms
+    float rightMotorTime;  //Time in ms
 
     @Override
     public void init(){
@@ -48,8 +31,10 @@ public class RecordAutonomous extends BasicHardware{
 
         filename = "autonomousrecordingtest.txt";
 
-        leftMotor = MotorState.INACTIVE;
-        rightMotor = MotorState.INACTIVE;
+        leftMotor = new Motor();
+        rightMotor = new Motor();
+
+
 
         driveLeft.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         driveRight.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
@@ -57,7 +42,10 @@ public class RecordAutonomous extends BasicHardware{
     @Override
 
     public void start(){
+        leftMotorTime = 0;
+        rightMotorTime = 0;
 
+        recording = "";
     }
     @Override
     public void loop() {
@@ -65,32 +53,32 @@ public class RecordAutonomous extends BasicHardware{
         driveLeft.setPower(-gamepad1.left_stick_y);
         driveRight.setPower(gamepad1.right_stick_y);
 
-        //Left Motor
-        // TODO test to make sure that these values line up with what the robot actually does.
-        if(driveLeft.getPower() > 0){
-            leftMotor = MotorState.FORWARD;
-        }
-        else if(driveLeft.getPower() < 0){
-            leftMotor = MotorState.BACKWARD;
-        }else{
-            leftMotor = MotorState.INACTIVE;
-        }
-        //Right Motor
-        if(driveRight.getPower() > 0){
-            rightMotor = MotorState.FORWARD;
-        }
-        else if(driveRight.getPower() < 0){
-            rightMotor = MotorState.BACKWARD;
-        }
-        else{
-            rightMotor = MotorState.INACTIVE;
-        }
 
-        if(leftMotor != MotorState.INACTIVE){
-            if(leftMotor == MotorState.FORWARD){
+        leftMotorTime++;
+        rightMotorTime++;
 
+        /*The idea here is that there will always be a time for each motor, so it would look something like this:
+          leftMotor : 1570 : INACTIVE
+          rightMotor : 10532 : FORWARD
+          leftMotor : 1054 : FORWARD
+          leftMotor : 3952 : BACKWARD
+
+          The reader will separate these statements into arrays of similar Motors (so a rightMotor array and a leftMotor array)
+          and loop through them at the same time.
+         */
+
+        //TODO figure out how to implement wheel rotations instead of times.
+        for(int i = 0; i < MotorState.values().length; i++){
+            if(leftMotor.motorState != leftMotor.lastTickMotorState){
+                recording += "leftMotor : " + leftMotorTime +  " : " + leftMotor.lastTickMotorState + "\n";
+                leftMotorTime = 0;
+            }
+            if(rightMotor.motorState != rightMotor.lastTickMotorState){
+                recording += "rightMotor : " + rightMotorTime + " : " + leftMotor.lastTickMotorState + "\n";
+                rightMotorTime = 0;
             }
         }
+
     }
     @Override
     public void stop(){
@@ -115,6 +103,63 @@ public class RecordAutonomous extends BasicHardware{
         }
 
         return recording;
+    }
+    private void TestMotorInput(){
+        //Left Motor
+        // TODO test to make sure that these values line up with what the robot actually does.
+        if(driveLeft.getPower() > 0){
+            leftMotor.motorState = MotorState.FORWARD;
+        }
+        else if(driveLeft.getPower() < 0){
+            leftMotor.motorState = MotorState.BACKWARD;
+        }else{
+            leftMotor.motorState = MotorState.INACTIVE;
+        }
+        //Right Motor
+        if(driveRight.getPower() > 0){
+            rightMotor.motorState = MotorState.FORWARD;
+        }
+        else if(driveRight.getPower() < 0){
+            rightMotor.motorState = MotorState.BACKWARD;
+        }
+        else{
+            rightMotor.motorState = MotorState.INACTIVE;
+        }
+    }
+
+    private class Motor {
+
+        MotorState motorState;
+        MotorState lastTickMotorState;
+
+        public Motor(){
+            motorState = MotorState.INACTIVE;
+        }
+
+        public MotorState MotorState(){
+            return motorState;
+        }
+    }
+
+    private enum MotorState {
+        FORWARD (0f),
+        BACKWARD (0f),
+        INACTIVE (0f);
+
+        private float time; //Time in milliseconds
+
+        private MotorState(float time){
+            this.time = time;
+        }
+
+        public void AddTime(float time){
+            this.time += time;
+        }
+
+        public float GetTime(){
+            return time;
+        }
+
     }
 
 }
