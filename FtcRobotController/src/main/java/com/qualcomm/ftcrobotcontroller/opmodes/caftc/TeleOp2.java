cf3e7d885add513b704b36332ea2504f987afa17
Caleb_Norfleet <cnorfleet0@gmail.com>
2016-02-11 10:32:24 -0500

    edits ands stuffs

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/TeleOp2.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/TeleOp2.java
@@ -1,17 +1,13 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.caftc;
 
-import com.qualcomm.robotcore.hardware.UltrasonicSensor;
-
 /**
  * Created by Caleb_Norfleet on 1/20/2016.
  */
 public class TeleOp2 extends BasicHardware{
     //Execution order : Start, Init, Loop, Stop
-    //driver 1
-    double sL; //ziplines
-    double sR;
+    //double sL; //ziplines
+    //double sR;
     double sC; //climber arm
-
     double rR; //rigid arms
     double rL;
     double sA; //servo arm angler base thingy
@@ -19,12 +15,12 @@ public class TeleOp2 extends BasicHardware{
     @Override
     public void init() {
         super.init();
-        sL = 0.4;
-        sR = 0.5;
-        sC = 0;
-        rR = 0.5;
-        rL = 0.5;
-        sA = 0.5;
+        //sL = 0;
+        //sR = 1;
+        sC = 1;
+        rR = 0.3;
+        rL = 0.3;
+        sA = 1;
     }
 
     @Override
@@ -41,41 +37,53 @@ public class TeleOp2 extends BasicHardware{
         //zip line right
         if (gamepad1.right_bumper) //right up
         {
-            if (sR - numZ > 0) { sR -= numZ; }
-            else { sR = 0; }
+            /*if (sR + numZ < 1) { sR += numZ; }
+            else { sR = 1; }*/
+            zipRight.setPosition(1);
         }
         else if (gamepad1.right_trigger > 0.5) //right down
         {
-            if (sR + numZ < 1) { sR += numZ; }
-            else { sR = 1; }
+            /*if (sR - numZ > 0) { sR -= numZ; }
+            else { sR = 0; }*/
+            zipRight.setPosition(0);
         }
-        telemetry.addData("zip right", sR);
-        zipRight.setPosition(sR);
+        else
+        {
+            zipRight.setPosition(0.5);
+        }
+        //telemetry.addData("zip right", sR);
+        //zipRight.setPosition(sR);
 
         //zip line left
         if (gamepad1.left_bumper) //left up
         {
-            if (sL + numZ < 1) { sL += numZ; }
-            else { sL = 1; }
+            /* if (sL - numZ > 0) { sL -= numZ; }
+            else { sL = 0; } */
+            zipLeft.setPosition(0);
         }
         else if (gamepad1.left_trigger > 0.5) //left down
         {
-            if (sL - numZ > 0) { sL -= numZ; }
-            else { sL = 0; }
+            /* if (sL + numZ < 1) { sL += numZ; }
+            else { sL = 1; } */
+            zipLeft.setPosition(1);
         }
-        telemetry.addData("zip left", sL);
-        zipLeft.setPosition(sL);
+        else
+        {
+            zipLeft.setPosition(0.5);
+        }
+        //telemetry.addData("zip left", sL);
+        //zipLeft.setPosition(sL);
 
         //climbers arm increment value
         double numC = 0.03;
 
         //climbers arm
-        if (gamepad1.dpad_up) //climbers up
+        if (gamepad1.dpad_down) //climbers down
         {
             if (sC + numC < 1) { sC += numC; }
             else { sC = 1; }
         }
-        else if (gamepad1.dpad_down) //climbers down
+        else if (gamepad1.dpad_up) //climbers up
         {
             if (sC - numC > 0) { sC -= numC; }
             else { sC = 0; }
@@ -94,9 +102,23 @@ public class TeleOp2 extends BasicHardware{
 
         //center spool angling - left joystick
         double speed = 0.01;
-        double left = gamepad2.left_stick_y;
-        if (right > .05 || right < -.05)
-        { sA += left * speed; }
+        double left = -gamepad2.left_stick_y;
+        if (left > .05)
+        {
+            if(sA + left * speed < 1)
+            { sA += left * speed; }
+            else
+            { sA = 1; }
+        }
+        else if (left < -.05)
+        {
+            if(sA + left * speed > 0)
+            { sA += left * speed; }
+            else
+            { sA = 0; }
+        }
+        armServo.setPosition(sA);
+        telemetry.addData("angle", sA);
 
         //rigid arm multiplier
         double rMult = 0.03;
@@ -112,6 +134,7 @@ public class TeleOp2 extends BasicHardware{
             if (rL - rMult > 0) { rL -= rMult; }
             else { rL = 0; }
         }
+        leftArm.setPosition(rL);
 
         //right rigid arm - right bumpers
         if (gamepad2.right_bumper) //right up
@@ -124,6 +147,7 @@ public class TeleOp2 extends BasicHardware{
             if (rR + rMult < 1) { rR += rMult; }
             else { rR = 1; }
         }
+        rightArm.setPosition(rR);
     }
 
     @Override
@@ -143,6 +167,9 @@ public class TeleOp2 extends BasicHardware{
         spoolMotor2.setPower(0);
 
         //CR_servo.setPosition(0.5);
+
+        zipLeft.setPosition(0.5);
+        zipRight.setPosition(0.5);
     }
 
     public void setSpoolPow(double d)
