f8b4d6a29c1dc598742731c325328d429e1b9f5a
jumbolimbo <thoffmann706@gmail.com>
2016-02-13 14:56:30 -0500

    Awesome Update

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/LIGHT_MODE.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/caftc/LIGHT_MODE.java
@@ -3,4 +3,16 @@ package com.qualcomm.ftcrobotcontroller.opmodes.caftc;
 /**
  * Created by Caleb_Norfleet on 2/13/2016.
  */
-public enum LIGHT_MODE{TRIGSQUARE_OSCILLATE_COLORS}
+public enum LIGHT_MODE{
+    TRIGSQUARE_OSCILLATE_COLORS(0);
+    private final int value;
+    private final int max = 1;
+    LIGHT_MODE(int i){
+        this.value=i;
+    }
+    private static LIGHT_MODE[] vals = values();
+    public LIGHT_MODE next()
+    {
+        return vals[(this.ordinal()+1) % vals.length];
+    }
+}
